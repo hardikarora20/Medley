60439b6d9e5e5ccfdc2a7272a192c3c810e96bdf
Hardik Arora <hardik.20.a@gmail.com>
2022-07-06 14:35:37 +0530

    v1.1

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 var t=1;
+var maxNo=40;
 // function real(){
 //     for(var i=6,j=33,k=60;i<28,j<55,k<83;i=i+3,j=j+3,k=k+3){
 //         document.getElementsByTagName("div")[j].style.animationDelay=""+j+t+"ms";
@@ -7,13 +8,127 @@ var t=1;
 //         document.getElementsByTagName("div")[i].style.animationDelay=""+i+t+"ms";
 //     }
 // }
+// localStorage.clear();
+let haveIt = [];
+function generateUniqueRandom(maxNr) {
+    //Generate random number
+    let random = (Math.random() * maxNr).toFixed();
 
+    //Coerce to number by boxing
+    random = Number(random);
+
+    if(!haveIt.includes(random)) {
+        haveIt.push(random);
+        return random;
+    } else {
+        if(haveIt.length < maxNr) {
+          //Recursively generate number
+         return  generateUniqueRandom(maxNr);
+        } else {
+          console.log('No more numbers available.')
+          return false;
+        }
+    }
+}
+
+function local(){
+    var k=1;
+    var local=0
+    for(var i=0;i<50;i++){
+        console.log(document.getElementsByTagName("div")[i].innerHTML=='Jump in');
+        if((document.getElementsByTagName("div")[i].innerHTML=='Jump in')){
+            local=parseInt(localStorage.getItem("last"+k+""));
+            if(localStorage.getItem("last"+k+"")==null){
+                document.getElementsByTagName("div")[i-3].style.display="none";
+            }
+            else{
+            document.getElementsByTagName("div")[i+1].innerText=data[local].singer;
+            document.getElementsByTagName("div")[i].innerText=data[local].name;    
+            document.getElementsByTagName("div")[i-2].style.backgroundImage='url('+data[local].image+')';
+            document.getElementsByTagName("div")[i-3].setAttribute("id",local);
+            k++;
+        }
+    }
+}
+}
+
+function recommend(){
+    var k=1;
+    var local=0
+    for(var i=50;i<100;i++){
+        if((document.getElementsByTagName("div")[i].innerHTML=='Recommended')){
+            local=generateUniqueRandom(maxNo);
+            document.getElementsByTagName("div")[i+1].innerText=data[local].singer;
+            document.getElementsByTagName("div")[i].innerText=data[local].name;    
+            document.getElementsByTagName("div")[i-2].style.backgroundImage='url('+data[local].image+')';
+            document.getElementsByTagName("div")[i-3].setAttribute("id",local);
+            k++;
+        }
+    }
+}
+
+console.log(localStorage.getItem("last1")=="0");
+        function localcheck(){
+            if(localStorage.getItem("last1")==null){
+                console.log("last null")
+            
+        document.getElementById("last-played").style.display="none";
+    }
+    else{
+        local();
+    }
+}
+function egg(){
+    setTimeout(function(){ 
+        document.getElementById("greeting").innerHTML="🥚 Music";
+    }, 1500);
+    document.getElementById("greeting").innerHTML="🐣 Music";
+}
+
+function setid(){
+    console.log("setting id");
+    var newid=0;
+    for(var i=0;i<334;i++){
+    if(document.getElementsByTagName("div")[i].innerText=='Song Name'){
+        document.getElementsByTagName("div")[i-3].setAttribute("id",parseInt(newid));    
+        newid++;
+    }
+    }
+}
+function menu(){
+    console.log("menu() called")
+    document.getElementById("menu-bar").classList.toggle("menuShow");
+}
+function removemenu(){
+    document.getElementById("menu-bar").classList.remove("menuShow");
+}
+function updatename(){
+    var username=" "+document.getElementById("username").value;
+    document.getElementById("greeting").innerText="Welcome";
+    document.getElementById("greeting").innerText+=username+",";
+    localStorage.setItem("username",username);
+}
+function getname(){
+    if(localStorage.getItem("username")!=null){
+    document.getElementById("greeting").innerText="Welcome back";
+    var username=localStorage.getItem("username");
+    document.getElementById("greeting").innerText+=username+",";
+}
+}
 function real(){
+    console.log(document.getElementsByTagName("div").length);
+    getname();
+    setid();
+    localcheck();
+    recommend();
+    for(var k=1;k<=8;k++){
+    console.log(localStorage.getItem("last"+k+""));
+    }
     var index=0;
-    for(var i=0,j=2;i<100;i++,j=j+4){
+    for(var i=0,j=2;i<334;i++,j=j+2){
         document.getElementsByTagName("div")[i].style.animationDelay=""+j+t+"ms";
         // t=t-0.1;
-        if((document.getElementsByTagName("div")[i].innerText=='Song Name')&&(index<24)){
+        if((document.getElementsByTagName("div")[i].innerText=='Song Name')&&(index<41)){
             document.getElementsByTagName("div")[i+1].innerText=data[index].singer;
             document.getElementsByTagName("div")[i].innerText=data[index].name;    
             document.getElementsByTagName("div")[i-2].style.backgroundImage='url('+data[index].image+')';
@@ -34,7 +149,8 @@ var intervalId = window.setInterval(function(){
         document.getElementById("change-play").innerHTML='<svg width="65" class="play" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 14L47.1667 32.75L18 51.5V14Z" fill="black" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';    document.getElementById("change-play-full").innerHTML='<svg class="full-play" width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M28.8333 16.3333H20.5V49.6666H28.8333V16.3333Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M45.5 16.3333H37.1667V49.6666H45.5V16.3333Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
         document.getElementById("change-play-full").innerHTML='<svg width="65" class="full-play" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 14L47.1667 32.75L18 51.5V14Z" fill="black" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';   
     }
-  }, 500);
+}, 500);
+var audio=document.getElementsByTagName("audio")[0];
 function play(){
     const audio=document.getElementsByTagName("audio")[0];
     if(document.getElementById("player").classList.contains("playing")){
@@ -50,15 +166,23 @@ function play(){
     document.getElementById("player").classList.toggle("playing");
 }
 var once=0;
-var idNow=0;
+var idNow=1;
+var currentname="Song";
+var currentinfo="Info";
+var currentimage="https://i.scdn.co/image/ab67616d0000b273459d675aa0b6f3b211357370";
 function now_play(id){
-    idNow=id;
+    console.log(id);
     document.title=data[id].name+' - '+data[id].singer;
     if(once==0){
         document.getElementById("music-panel").classList.toggle("visible");
+        document.getElementById("page").classList.toggle("padding");
         once++;
     }
     // document.getElementById("seek-audio").max=document.getElementById("player").duration;
+    currentname=data[id].name;
+    currentinfo=data[id].singer;
+    currentimage=data[id].image;
+    medias();
     document.getElementById("current").innerText=data[id].name;
     document.getElementById("current-singer").innerText=data[id].singer;
     document.getElementById("now-image").style.backgroundImage='url('+data[id].image+')';
@@ -69,33 +193,43 @@ function now_play(id){
     document.getElementById("full-now-cover").style.backgroundImage='url('+data[id].image+')';
     document.getElementsByTagName("audio")[0].setAttribute("src",data[id].link);
     document.getElementsByTagName("audio")[0].play();
+    localStorage.setItem("last"+idNow+"",id); 
+    idNow++;
+}
+function updateVar(){
+    currentname=document.getElementById("current").innerHTML;
+    currentinfo=document.getElementById("current-singer").innerHTML;
+    console.log("currentinfo");
 }
 
 // if ('mediaSession' in navigator) {
-
+//     // currentimage=document.getElementById("now-image").innerHTML;
+// //     audio.currentTime = details.seekTime;
+//     updateVar();
 //     navigator.mediaSession.metadata = new MediaMetadata({
-//     title: 'Never Gonna Give You Up',
-//     artist: 'Rick Astley',
-//     album: 'Whenever You Need Somebody',
+//     title: currentname,
+//     artist: currentinfo,
 //     artwork: [
-//         { src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
-//         { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
-//         { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
-//         { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
-//         { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
-//         { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
+// //         { src: currentimage, sizes: '96x96',   type: 'image/png' },
+// //         { src: currentimage, sizes: '128x128', type: 'image/png' },
+// //         { src: currentimage, sizes: '192x192', type: 'image/png' },
+// //         { src: currentimage, sizes: '256x256', type: 'image/png' },
+// //         { src: currentimage, sizes: '384x384', type: 'image/png' },
+//         { src: currentimage, sizes: '512x512', type: 'image/png' },
 //     ]
 //     });
 
-//     navigator.mediaSession.setActionHandler('play', function() {});
-//     navigator.mediaSession.setActionHandler('pause', function() {});
-//     navigator.mediaSession.setActionHandler('seekbackward', function() {});
-//     navigator.mediaSession.setActionHandler('seekforward', function() {});
+//     navigator.mediaSession.setActionHandler('play', function() {audio.play();});
+//     navigator.mediaSession.setActionHandler('pause', function() {audio.pause();});
+//     navigator.mediaSession.setActionHandler('seekbackward', function() {seekbackward();});
+//     navigator.mediaSession.setActionHandler('seekforward', function() {seekforward();});
 //     navigator.mediaSession.setActionHandler('previoustrack', function() {});
 //     navigator.mediaSession.setActionHandler('nexttrack', function() {});
+
 // }
 function show(){
     document.getElementById("full-player").classList.toggle("visible");
+    document.getElementById("full-black").classList.toggle("visible");
     // if(document.getElementById("full-player").classList.contains("visible")){
     //     document.documentElement.requestFullscreen();
     // }
@@ -103,6 +237,51 @@ function show(){
     //     document.exitFullscreen();
     // }
 }
+console.log(test());
+function test(){
+    return document.getElementById("current").innerHTML;
+}
+function medias(){
+    const audio=document.getElementsByTagName("audio")[0];
+    console.log("called");
+    if ('mediaSession' in navigator) {
+        const events=["durationchange","timechange"];
+        for(const event in events)
+        audio.addEventListener(event,updatemediasessionstate);
+        navigator.mediaSession.metadata = new MediaMetadata({
+          title: currentname,
+          artist: currentinfo,
+          artwork: [
+            { src: currentimage, sizes: '96x96',   type: 'image/png' },
+            { src: currentimage, sizes: '128x128', type: 'image/png' },
+            { src: currentimage, sizes: '192x192', type: 'image/png' },
+            { src: currentimage, sizes: '256x256', type: 'image/png' },
+            { src: currentimage, sizes: '384x384', type: 'image/png' },
+            { src: currentimage, sizes: '512x512', type: 'image/png' },
+          ]
+          
+        });
+        
+        function  updatemediasessionstate(){
+            navigator.mediaSession.setPositionState({
+          duration: audio.duration,
+          position: audio.currentTime,
+        });
+    }
+        navigator.mediaSession.setActionHandler('play', function() { audio.play();/* Code excerpted. */ });
+        navigator.mediaSession.setActionHandler('pause', function() { audio.pause();/* Code excerpted. */ });
+        navigator.mediaSession.setActionHandler('stop', function() { /* Code excerpted. */ });
+        navigator.mediaSession.setActionHandler('seekbackward', function() { seekbackward();/* Code excerpted. */ });
+        navigator.mediaSession.setActionHandler('seekforward', function() { seekforward();/* Code excerpted. */ });
+        // navigator.mediaSession.setActionHandler('seekto', function() { /* Code excerpted. */ });
+        // navigator.mediaSession.setActionHandler('previoustrack', function() { /* Code excerpted. */ });
+        // navigator.mediaSession.setActionHandler('nexttrack', function() { /* Code excerpted. */ });
+        // navigator.mediaSession.setActionHandler('skipad', function() { /* Code excerpted. */ });
+      }
+      
+}
+
+
 var img = document.createElement('img');
 img.setAttribute('src', 'examples/octocat.png')
 
@@ -176,5 +355,8 @@ function songmute(){
     }
 }
 function next(){
-    now_play(idNow+1);
+    if(((idNow+1)!=7)||((idNow+1)!=15)||((idNow+1)!=23)){
+        now_play(idNow+1);
+    }
+
 }
---
+++ songs.js
@@ -0,0 +1,334 @@
+var data=[{
+    "name"   : "Let Me Down Slowly",
+    //"id"     : "1",
+    "singer" : "Alec Benjamin" ,
+    "type"   : "pop" ,
+    "link"   : "https://sdlhivkcdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/681/724686666140f0e0249acff646210917_320.mp4" , 
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273459d675aa0b6f3b211357370" 
+},
+{
+    "name"   : "Unstoppable",
+    //"id"     : "2",
+    "singer" : "Sia" , 
+    "type"   : "pop" ,
+    "link"   : "https://sdlhivkecdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/552/26b33054461788c6282e4ac814d3769f_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27349e0134c686547c28b7c999f" 
+},
+{
+    "name"   : "One Kiss",
+    //"id"     : "3",
+    "singer" : "Dua Lipa, Calvin Harris" ,
+    "type"   : "pop" ,
+    "link"   : "https://sdlhivkcdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/360/b35084c623cc8a39d72e4cf10f9f895c_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273d09f96d82310d4d77c14c108" 
+},    
+{
+    "name"   : "Blinding Lights",
+    //"id"     : "4",
+    "singer" : "The Weeknd" ,
+    "type"   : "pop" ,
+    "link"   : "https://sklktcdnems07.cdnsrv.jio.com/jiosaavn.cdn.jio.com/167/7d38d0a6d8bb2decb2ffcfc648f4d835_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b2738863bc11d2aa12b54f5aeb36" 
+},
+{
+    "name"   : "Night Changes",
+    //"id"     : "5",
+    "singer" : "One Direction" ,
+    "type"   : "pop" ,
+    "link"   : "https://sklktcdnems05.cdnsrv.jio.com/jiosaavn.cdn.jio.com/851/d3f6d5e7ae7c9050697a779f85eaabe8_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273d304ba2d71de306812eebaf4" 
+
+}, 
+{
+    "name"   : "Lovely",
+    //"id"     : "6",
+    "singer" : "Billie Eilish, Khalid" ,
+    "type"   : "pop" ,
+    "link"   : "https://sdlhivkcdnems05.cdnsrv.jio.com/jiosaavn.cdn.jio.com/947/580fe2c0cb03ba147b00a4b2261764d4_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b2738a3f0a3ca7929dea23cd274c" 
+
+},  
+{
+    "name"   : "Positions",
+    //"id"     : "7",
+    "singer" : "Ariana Grande" ,
+    "type"   : "pop" ,
+    "link"   : "https://sklktcdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/894/9b829545e82fbee213ce0e31f6e7b785_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b2735ef878a782c987d38d82b605" 
+},  
+{
+    "name"   : "Levitating",
+    //"id"     : "8",
+    "singer" : "Dua Lipa, Sean Paul" ,
+    "type"   : "pop" ,
+    "link"   : "https://sklktecdnems06.cdnsrv.jio.com/jiosaavn.cdn.jio.com/651/1961c02c707b866858ce1db3ce304d5c_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273bd26ede1ae69327010d49946" 
+},  
+{
+    "name"   : "Pee Loon",
+    //"id"     : "1",
+    "singer" : "Pritam, Mohit Chauhan" ,
+    "type"   : "bolly" ,
+    "link"   : "https://snoidcdnems07.cdnsrv.jio.com/jiosaavn.cdn.jio.com/609/b9b2c6dad98bdf89b1368acf6ab4647d_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273df934f4de5f8dd46f8d75f5f" 
+},  
+{
+    "name"   : "Piya O Re Piya",
+    //"id"     : "2",
+    "singer" : "Atif Aslam, Shreya Ghoshal" ,
+    "type"   : "bolly" ,
+    "link"   : "https://pwdown.com/113048/01.%20Piya%20O%20Re%20Piya.mp3" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b2731186688408c0a68041dc74d4" 
+},  
+{
+    "name"   : "Rabba Main Toh Mar Gaya Oye",
+    //"id"     : "3",
+    "singer" : "Pritam, Rahat Fateh Ali Khan" ,
+    "type"   : "bolly" ,
+    "link"   : "https://sdlhivkecdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/173/818c46f3740c64ad77906700f642c6c9_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273921798bf86a4d458a45f680a" 
+},
+{
+    "name"   : "Tere Sang Yaara",
+    //"id"     : "4",
+    "singer" : "Atif Aslam" ,
+    "type"   : "bolly" ,
+    "link"   : "https://sklktecdnems06.cdnsrv.jio.com/jiosaavn.cdn.jio.com/298/74e78bbd04353f2fb7059c4b41ec15b1_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273c202064e0775440dacef8802" 
+},
+{
+    "name"   : "Meherbani",
+    //"id"     : "5",
+    "singer" : "Arko, Jubin Nautiyal" ,
+    "type"   : "bolly" ,
+    "link"   : "https://snoidcdnems06.cdnsrv.jio.com/jiosaavn.cdn.jio.com/995/719fd938e749f26dc991a0e84454fafc_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273dae669e0bc817fd2d2235fa6" 
+},
+{
+    "name"   : "Mileya Mileya",
+    //"id"     : "6",
+    "singer" : "Rekha Bhardwaj, Jigar Saraiya, Priya Andrews" ,
+    "type"   : "bolly" ,
+    "link"   : "https://snoidcdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/523/02a57c015cd177a1661bcb99c033d983_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27351c62d5e6b6606bb2aa8d0e2" 
+},
+{
+    "name"   : "Mitwa",
+    //"id"     : "7",
+    "singer" : "Caralisa Monteiro, Shankar Mahadevan, Shafqat Amanat Ali, Shankar Ehsaan Loy" ,
+    "type"   : "bolly" ,
+    "link"   : "https://snoidcdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/089/02338877de7abe1e170818893e2c992a_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273afbf50bfb7b032b69deabade" 
+},
+{
+    "name"   : "Khula Aasmaan",
+    //"id"     : "8",
+    "singer" : "KK, Salim Merchant, Salim-Sulaiman" ,
+    "type"   : "bolly" ,
+    "link"   : "https://pwdown.com/110570/02.%20Khulla%20Aasman.mp3" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b2737f8ec826d2b27c71f60da168" 
+},
+{
+    "name"   : "Shaayraana",
+    //"id"     : "9",
+    "singer" : "Arijit Singh , Pritam" ,
+    "type"   : "bolly" ,
+    "link"   : "https://sklktcdnems05.cdnsrv.jio.com/jiosaavn.cdn.jio.com/166/9cf0ae1346867c3e24fd25b7b27256ad_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27367ba10f66efcbe557307d57b" 
+},
+{
+    "name"   : "As It Was",
+    //"id"     : "8",
+    "singer" : "Harry Styles" ,
+    "type"   : "popular" ,
+    "link"   : "https://sklktcdnems07.cdnsrv.jio.com/jiosaavn.cdn.jio.com/720/ae1594f81fac092f502317bc4e25a5b2_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b2732e8ed79e177ff6011076f5f0" 
+},
+{
+    "name"   : "Willow",
+    //"id"     : "1",
+    "singer" : "Taylor Swift" ,
+    "type"   : "popular" ,
+    "link"   : "https://sdlhivkecdnems04.cdnsrv.jio.com/jiosaavn.cdn.jio.com/675/0330684c945a7ac8a1191f12cac6d507_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27333b8541201f1ef38941024be" 
+},
+{
+    "name"   : "Dusk Till Dawn (feat. Sia)",
+    //"id"     : "3",
+    "singer" : "ZAYN, Sia" ,
+    "type"   : "popular" ,
+    "link"   : "https://sklktcdnems05.cdnsrv.jio.com/jiosaavn.cdn.jio.com/896/ad19c4a75775ccdef32905de23fc10c5_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27323852b7ef0ecfe29d38d97ee" 
+},
+{
+    "name"   : "Counting Stars",
+    //"id"     : "4",
+    "singer" : "OneRepublic" ,
+    "type"   : "popular" ,
+    "link"   : "https://snoidcdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/574/4d0f563256494b05146824bf45cd94e4_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b2739e2f95ae77cf436017ada9cb" 
+},
+{
+    "name"   : "Running Up That Hill (A Deal With God)",
+    //"id"     : "7",
+    "singer" : "Kate Bush" ,
+    "type"   : "popular" ,
+    "link"   : "https://sdlhivkecdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/503/f4c37ed7f6b52154c25c57124b184f2d_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27396ab64f52273635308b6bf27" 
+},
+{
+    "name"   : "Strawberries And Cigarettes",
+    //"id"     : "5",
+    "singer" : "Troye Sivan" ,
+    "type"   : "popular" ,
+    "link"   : "https://snoidcdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/883/8ff1cb5c5bc50cf85052fbdf8195c295_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273c9eb4c87e1d7f5353908b712" 
+},
+{
+    "name"   : "We Don't Talk Anymore",
+    //"id"     : "6",
+    "singer" : "Charlie Puth, Selena Gomez" ,
+    "type"   : "popular" ,
+    "link"   : "https://pagalworld.nl/files/download/id/12374" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273633a2d775747bccfbcb17a45" 
+},
+{
+    "name"   : "Watermelon Sugar",
+    //"id"     : "2",
+    "singer" : "Harry Styles" ,
+    "type"   : "popular" ,
+    "link"   : "https://sdlhivkecdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/213/b65310797c4153ff708343a8ee4af9de_320.mp4" ,
+    "image"  : "https://i.pinimg.com/736x/38/21/94/382194fbaf52fe4a3242fc96aa34fd34.jpg" 
+},
+{
+    "name"   : "Akhiyaan",
+    //"id"     : "1",
+    "singer" : "Mitraz" ,
+    "type"   : "indie" ,
+    "link"   : "https://sdlhivkecdnems04.cdnsrv.jio.com/jiosaavn.cdn.jio.com/733/473355e0c914b85e48820b54978ccc9e_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273dde7939fb4a77cdd4a814249" 
+},
+{
+    "name"   : "Rangi Saari",
+    //"id"     : "2",
+    "singer" : "Kavita Seth, Kanishk Seth" ,
+    "type"   : "indie" ,
+    "link"   : "https://snoidcdnems07.cdnsrv.jio.com/jiosaavn.cdn.jio.com/887/9a81462b89e578a1011562fe6f424c58_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27368306b9b495f303159a892e3" 
+},
+{
+    "name"   : "Aankhon Se Batana",
+    //"id"     : "3",
+    "singer" : "Dikshant" ,
+    "type"   : "indie" ,
+    "link"   : "https://sklktcdnems04.cdnsrv.jio.com/jiosaavn.cdn.jio.com/620/9e31bdd6f14fd647482d59d621b7abdf_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27379b28ed7035877041d7b9770" 
+},
+{
+    "name"   : "Kasoor",
+    //"id"     : "4",
+    "singer" : "Prateek Kuhad" ,
+    "type"   : "indie" ,
+    "link"   : "https://sklktecdnems06.cdnsrv.jio.com/jiosaavn.cdn.jio.com/036/5ac1b1f1898a2e772191079ae1186e4c_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273ae7abe97f7020d657e87bbec" 
+},
+{
+    "name"   : "Baarishein",
+    //"id"     : "5",
+    "singer" : "Anuv Jain" ,
+    "type"   : "indie" ,
+    "link"   : "https://snoidcdnems04.cdnsrv.jio.com/jiosaavn.cdn.jio.com/520/060f65240088865166a061f2942ef92e_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27361694f071d402a10931d671e" 
+},
+{
+    "name"   : "Chaand Baaliya",
+    //"id"     : "6",
+    "singer" : "Aditya A" ,
+    "type"   : "indie" ,
+    "link"   : "https://sklktecdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/654/6c7e5af76004b142e37a57fcbba9bb3b_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273ec4ecaa0fcf68c0966f21514" 
+},
+{
+    "name"   : "Baaraat",
+    //"id"     : "7",
+    "singer" : "Ritviz, Nucleya" ,
+    "type"   : "indie" ,
+    "link"   : "https://snoidcdnems06.cdnsrv.jio.com/jiosaavn.cdn.jio.com/298/57972fe15db6ba877bc14481aef27214_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b2734d251de9c169a4af8b65a3cd" 
+},
+{
+    "name"   : "Khwaab",
+    //"id"     : "8",
+    "singer" : "Anumita Nadesan" ,
+    "type"   : "indie" ,
+    "link"   : "https://sklktcdnems07.cdnsrv.jio.com/jiosaavn.cdn.jio.com/754/62471e32f035022a585851f28fb97429_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b2734127ca62352cd5af92e10457" 
+},
+{
+    "name"   : "Darshana",
+    //"id"     : "1",
+    "singer" : "Hesham Abdul Wahab, Darshana Rajendran" ,
+    "type"   : "tolly" ,
+    "link"   : "https://snoidcdnems06.cdnsrv.jio.com/jiosaavn.cdn.jio.com/371/15a6b73ac92c980d8e3643b1bf18a933_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27380cdfb78965c63f318709811" 
+},
+{
+    "name"   : "Kannil Kannil",
+    //"id"     : "2",
+    "singer" : "Haricharan, Sayanora Philip" ,
+    "type"   : "tolly" ,
+    "link"   : "https://sdlhivkcdnems04.cdnsrv.jio.com/jiosaavn.cdn.jio.com/096/cc6557970c27c96fb3d456bd0416eccf_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273e36f27b29a09d05564ba21f2" 
+},
+{
+    "name"   : "Kattu Mooliyo",
+    //"id"     : "3",
+    "singer" : "Vineeth Sreenivasan, Shaan Rahman" ,
+    "type"   : "tolly" ,
+    "link"   : "https://sdlhivkecdnems04.cdnsrv.jio.com/jiosaavn.cdn.jio.com/118/9998639ed420402e97b8a8b6d507e1f8_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27370fff9e83c9ac021091c6ab0" 
+},
+{
+    "name"   : "Vennelave Vennelave",
+    //"id"     : "4",
+    "singer" : "Hariharan, Sadhana Sargam" ,
+    "type"   : "tolly" ,
+    "link"   : "https://snoidcdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/485/28d223dd762ff6dc232df2e2ed05e4a4_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27384351604b643064b97982db5" 
+},
+{
+    "name"   : "Pavizha Mazha",
+    //"id"     : "5",
+    "singer" : "KS Harisankar" ,
+    "type"   : "tolly" ,
+    "link"   : "https://snoidcdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/485/28d223dd762ff6dc232df2e2ed05e4a4_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b27398f93b54d337716f370fde19" 
+},
+{
+    "name"   : "Minni Minni",
+    //"id"     : "6",
+    "singer" : "Amritha Suresh" ,
+    "type"   : "tolly" ,
+    "link"   : "https://sdlhivkecdnems06.cdnsrv.jio.com/jiosaavn.cdn.jio.com/564/aaca93dc539f6ec0a1595f4798e0350a_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273858d8d7d80d83cd091c3d9f9" 
+},
+{
+    "name"   : "Ee Khalbitha",
+    //"id"     : "7",
+    "singer" : "Suchith Sureshan" ,
+    "type"   : "tolly" ,
+    "link"   : "https://sdlhivkecdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/924/9a5f803342e38ceedf7405734ad4ef6f_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273309f07efe994d37d93800c55" 
+},
+{
+    "name"   : "Kalaavathi",
+    //"id"     : "8",
+    "singer" : "Thaman S, Sid Sriram" ,
+    "type"   : "tolly" ,
+    "link"   : "https://sdlhivkcdnems05.cdnsrv.jio.com/jiosaavn.cdn.jio.com/250/d067c88312cbc32cc708e20ff55f4360_320.mp4" ,
+    "image"  : "https://i.scdn.co/image/ab67616d0000b273628c64b83f6ff72b9f472699" 
+},
+];
+
+
+
